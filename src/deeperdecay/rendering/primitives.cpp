575de920fd4f133bded6477584207d9130480248
Nuclaer <dylanbrophy@gmail.com>
2021-10-21 11:14:20 -0400

    wip for easy drawing of graphics primitives

---
+++ src/deeperdecay/rendering/primitives.cpp
@@ -0,0 +1,4 @@
+//
+// Created by nuclaer on 10/20/21.
+//
+
---
+++ src/deeperdecay/rendering/primitives.h
@@ -0,0 +1,8 @@
+//
+// Created by nuclaer on 10/20/21.
+//
+
+#ifndef DEEPERDECAY_PRIMITIVES_H
+#define DEEPERDECAY_PRIMITIVES_H
+
+#endif //DEEPERDECAY_PRIMITIVES_H
